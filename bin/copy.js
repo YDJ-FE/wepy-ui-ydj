bdb6cd2518c6233786bfcc91c1fd46d5f146de7e
王世隆 <547463736@qq.com>
2018-06-05 16:29:14 +0800

    init

---
+++ bin/copy.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// var fs = require('fs')
+
+// function copy(src, dst) {
+//   fs.createReadStream(src).pipe(fs.createWriteStream(dst))
+// }
+
+// function main(argv) {
+//   copy(argv[0], argv[1])
+// }
+
+// main(process.argv.slice(2))
